bbbf44b05c5cd983f93f95c6e3ee0c8d8acb7005
elyassdam <yassin@iesvalmayor.es>
2025-05-11 08:22:25 -0400

    11

--- includes/ft_push_swap.h
+++ includes/ft_push_swap.h
@@ -1,42 +1,52 @@
 // ft_push_swap.h
 #ifndef FT_PUSH_SWAP_H
-#define FT_PUSH_SWAP_H
+# define FT_PUSH_SWAP_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include "../libft/libft.h" // Para ft_atoi, ft_split, etc.
+# include "../libft/libft.h" // Para ft_atoi, ft_split, etc.
+# include <stdlib.h>
+# include <unistd.h>
 
 // Structures
 typedef struct s_node
 {
-    int value;
-    struct s_node *next;
-} t_node;
+	int				value;
+	struct s_node	*next;
+}					t_node;
 
 typedef struct s_stack
 {
-    t_list *top;
-    int size;
-    char stack_id; 
-} t_stack;
+	t_list			*top;
+	int				size;
+	char			stack_id;
+}					t_stack;
 
 // Function prototypes
-void    sa(t_stack *a);
-void    sb(t_stack *b);
-void    ss(t_stack *a, t_stack *b);
-void    pa(t_stack *a, t_stack *b);
-void    pb(t_stack *a, t_stack *b);
-void    ra (t_stack *a);
-void    rb (t_stack *b);
-void    rr (t_stack *a, t_stack *b);
-void    rra (t_stack *a);
-void    rrb (t_stack *b);
-void    rrr (t_stack *a, t_stack *b);
-int parse_args(t_stack *stack, int argc, char **argv);
-int ft_check_duplicates(t_stack *stack);
-void ft_error(void); // Si tienes una función ft_error
-void ft_lstadd_back_int(t_stack *stack, int value); // Si la usas aquí
-void ft_print_stack(t_stack *stack, char stack_name); // Si la usas aquí
-void sort_three(t_stack *a);
-void sort_five(t_stack *a, t_stack *b);
+void				sa(t_stack *a);
+void				sb(t_stack *b);
+void				ss(t_stack *a, t_stack *b);
+void				pa(t_stack *a, t_stack *b);
+void				pb(t_stack *a, t_stack *b);
+void				ra(t_stack *a);
+void				rb(t_stack *b);
+void				rr(t_stack *a, t_stack *b);
+void				rra(t_stack *a);
+void				rrb(t_stack *b);
+void				rrr(t_stack *a, t_stack *b);
+int					parse_args(t_stack *stack, int argc, char **argv);
+int					ft_check_duplicates(t_stack *stack);
+void	ft_error(void);                                 
+		// Si tienes una función ft_error
+void	ft_lstadd_back_int(t_stack *stack, int value);   // Si la usas aquí
+void	ft_print_stack(t_stack *stack, char stack_name); // Si la usas aquí
+void				sort_three(t_stack *a);
+void				sort_five(t_stack *a, t_stack *b);
+// aux
+int					get_max(t_stack *a);
+int					get_min(t_stack *a);
+int					get_pos(t_stack *a, int value);
+int					cost(t_stack *a, t_stack *b, int value);
+int					cheapest(t_stack *a, t_stack *b);
+int					find_couple(t_stack *b, int value_a);
+int					get_moves_to_top(t_stack *stack, int value);
+int					do_move_top(t_stack *a, t_stack *b);
 #endif
--- src/sort/sort_five.c
+++ src/sort/sort_five.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   sort_five.c                                        :+:      :+:    :+:   */
@@ -6,9 +6,9 @@
 /*   By: yael-you <yael-you@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 14:30:38 by yael-you          #+#    #+#             */
-/*   Updated: 2025/05/08 02:44:42 by yael-you         ###   ########.fr       */
+/*   Updated: 2025/05/11 08:00:40 by yael-you         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "../includes/ft_push_swap.h"
 
@@ -84,125 +84,4 @@ void sort_five(t_stack *a, t_stack *b)
     ra(a);
     pa(a,b);
     ra(a);
-}
-void push_two(t_stack *a, t_stack *b)
-{
-    if (!a || !a->top || !a->top->next)
-        return;
-    pb(a,b);
-    pb(a,b);
-}
-int cost(t_stack *a, t_stack *b, int value)
-{     if (!a || !a->top || !b || !b->top)
-        return __INT_MAX__; 
-    int moves_a;
-    int couple;
-    int moves_b;
-    int cost;
-    moves_a = get_moves_to_top(a, value);
-    couple = find_couple(b , value);
-    moves_b = get_moves_to_top(b, couple);
-    
-if ((moves_a >= 0 && moves_b >= 0) || (moves_a < 0 && moves_b < 0))
-{
-    if (moves_a > moves_b)
-        cost = moves_a;
-    else
-        cost = moves_b;
-}
-else
-{
-    if (moves_a < 0)
-        moves_a = -moves_a;
-    if (moves_b < 0)
-        moves_b = -moves_b;   
-    cost = moves_a + moves_b;
-}
-return cost;
-}
-int cheapest(t_stack *a, t_stack *b)
-{
-    if (!a || !a->top)
-        return 0;
-    t_list *i;
-    int cheap;
-    int cheapest_cost;
-    int cheapest_value;
-    int value;
-    int current_cost;
-
-    i = a->top;
-    cheapest_value = *(int *)i->content;
-    cheapest_cost = cost(a,b,cheapest_value);
-    
-    while (i != NULL)
-    {
-        value = *(int *) i->content;
-        current_cost = cost(a,b,value);
-        if (current_cost < cheapest_cost)
-        {
-            cheapest_cost = current_cost;
-            cheapest_value = value;        
-        }
-    i = i->next;    
-}
-return cheapest_value;
-}
-int find_couple(t_stack *b, int value_a)
-{
-    t_list *i;
-    int maxb;
-    int minb;
-    int value_b;
-    int diff;
-    int min_diff;
-    int best_match;
-    
-    best_match = 0;
-    min_diff = __INT_MAX__;
-    if (!b || !b->top)
-        return 0;
-    maxb = get_max(b);
-    minb = get_min(b);
-    if (value_a > maxb || value_a < minb)
-        return minb;
-    best_match = minb;
-    i = b->top;
-    while(i != NULL)
-    {
-        value_b = *(int *)i->content;
-        diff = value_a -value_b;
-        if (diff > 0 && diff < min_diff)
-        {   min_diff = diff;
-            best_match = value_b;
-        }
-        i = i->next;
-    }
-    return best_match;
-}
-  
-int get_moves_to_top(t_stack *stack, int value) {
-    if (!stack || !stack->top)
-        return -1;
-    t_list *i; 
-    int index;
-    int moves_up;
-    int moves_down;
-    i = stack->top;
-    index = 0;
-    while (i != NULL)
-    {
-        if (*(int *)i->content == value)
-        {
-            moves_up = index;
-            moves_down = stack->size - index; 
-            if (moves_up <= moves_down)
-                return moves_up;
-            else 
-                return -moves_down;
-        }
-        i = i->next;
-        index ++;
-    }
-    return -1;
 }
